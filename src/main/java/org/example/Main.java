eba91ee7a14b0b8d0670cf93a9ffc2d68cbedc73
Aakriti <aakrititiwari002@gmail.com>
2025-01-05 13:06:14 +0530

    uc2 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,23 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        HashTable<String, Integer> HTable = new HashTable<>();
+        HashTable<String, Integer> hashTable = new HashTable<>();
         String sentence = "To be or not to be";
         for (String word : sentence.split(" ")) {
-            Integer count = HTable.get(word);
+            Integer count = hashTable.get(word);
             count = (count == null) ? 1 : count + 1;
-            HTable.put(word, count);
+            hashTable.put(word, count);
         }
-        System.out.println(HTable);
+        System.out.println(hashTable);
+
+
+        String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        for (String word : paragraph.split(" ")) {
+            Integer count = hashTable.get(word);
+            count = (count == null) ? 1 : count + 1;
+            hashTable.put(word, count);
+        }
+        System.out.println(hashTable);
+
     }
 }
