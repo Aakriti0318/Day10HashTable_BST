c89d4d6652b19995caf07ab0632b1ac2dd48ac22
Aakriti <aakrititiwari002@gmail.com>
2025-01-05 13:04:20 +0530

    uc1 commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        HashTable<String, Integer> HTable = new HashTable<>();
+        String sentence = "To be or not to be";
+        for (String word : sentence.split(" ")) {
+            Integer count = HTable.get(word);
+            count = (count == null) ? 1 : count + 1;
+            HTable.put(word, count);
+        }
+        System.out.println(HTable);
+    }
+}
